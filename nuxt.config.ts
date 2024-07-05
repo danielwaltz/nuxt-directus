68ae688db269a486cccfd0f9df6d4f2c80acce99
Daniel Waltz <daniel.waltz@moserit.com>
2024-07-05 12:46:45 -0400

    refactor: use nuxt 4 directory structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-07-04',
   modules: ['@nuxt/eslint', '@unocss/nuxt'],
+  future: {
+    compatibilityVersion: 4,
+  },
   experimental: {
     asyncContext: true,
-    headNext: true,
     typedPages: true,
     viewTransition: true,
   },
